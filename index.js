95ab2d5c73bb228cc8a93ad4882576624644ba6b
rahmaidle <rahmaidle254@gmail.com>
2024-06-26 12:16:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+};
+
+console.log("The answer to the first question is: " + titleCased()[0]);
